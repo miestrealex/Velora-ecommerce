2f45a3d193073fdf81b8e69fe38f138c6f31dfd7
Alex <miestrealex@gmail.com>
2026-06-04 18:07:20 +0100

    Reorganize admin dashboard structure, tabs and CSS

--- admin.php
+++ admin.php
@@ -68,7 +68,7 @@ if (isset($_POST["add-category"])) {
     <title>Velora Admin</title>
     <link rel="stylesheet" href="admin.css">
     <link rel="stylesheet" href="style.css">
-</head>
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" </head>
 
 <body>
     <header class="admin-header">
@@ -85,51 +85,60 @@ if (isset($_POST["add-category"])) {
             <div id="admin-tabs">
                 <div id="search-wrapper">
                     <button onclick="Showtab('search-products')">
+                        <i class="fa-solid fa-magnifying-glass">
+                            &nbsp;
+                        </i>
                         Search Products
+
                     </button>
                 </div>
                 <div id="add-product-wrapper">
                     <button onclick="Showtab('add-product')">
+                        <i class="fa-solid fa-plus">
+                            &nbsp;
+                        </i>
                         Add Product
                     </button>
                 </div>
                 <div id="add-category-wrapper">
                     <button onclick="Showtab('add-category')">
+                        <i class="fa-solid fa-folder-plus">
+                            &nbsp;
+                        </i>
                         Add Category
                     </button>
                 </div>
                 <div id="categories-wrapper">
                     <button onclick="Showtab('categories')">
+                        <i class="fa-solid fa-list">
+                            &nbsp;
+                        </i>
                         Category
                     </button>
                 </div>
             </div>
-            <div id="search-products" class="admin-section">
-                <h2>Search Product</h2>
+            <div id="tabs-content" class="admin-sections">
+
+                <!---interior do botao search---->
+                <div id="search-products" class="admin-section">
+                    <h2 class="section-title">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        &nbsp;Search Product
+                    </h2>
                     <form method="GET">
                         <input type="text" name="search" placeholder="Search Product...">
                         <button type="submit">
                             Search
                         </button>
                     </form>
-            </div>
-            <div id="categories" class="admin-section">
-                <h2>Filter Category</h2>
-                <div class="admin-categories">
-                    <a href="admin.php">All Products</a>
-                    <?php $categories->data_seek(0);
-                    while ($category = $categories->fetch_assoc()) {
-                        ?>
-                        <a href="admin.php?category=<?php echo $category['id']; ?>">
-                            <?php echo $category['name']; ?>
-                        </a>
-                    <?php } ?>
                 </div>
-            </div>
 
-            <div class="admin-manage">
-                <div id="add-product" class="admin-section admin-add-product">
-                    <h2>Add Product</h2>
+                <!---interior do botao add-product---->
+                <div id="add-product" class="admin-section">
+                    <h2 class="section-title">
+                        <i class="fa-solid fa-plus"></i>
+                        &nbsp;Add Product
+                    </h2>
                     <form action="" method="POST">
                         <input type="text" name="name" placeholder="Product Name" required>
                         <select name="category_id" required>
@@ -160,130 +169,173 @@ if (isset($_POST["add-category"])) {
                         </button>
                     </form>
                 </div>
-            </div>   
-                <div id="add-category" class="admin-section admin-add-category">
+
+                <!---interior do botao add-category---->
+                <div id="add-category" class="admin-section">
                     <h2>Add Category</h2>
-                    <p>
-                        <?php
-                        if (isset($_GET['error']) && $_GET['error'] == 'exists') {
-                            echo "<p> Category already exists.</p>";
-                        }
-                        ?>
+                    <?php
+                    if (isset($_GET['error']) && $_GET['error'] == 'exists') {
+                        echo "<p> Category already exists.</p>";
+                    }
+                    ?>
                     <form method="POST">
                         <input type="text" name="category-name" placeholder="Category Name" required>
                         <button type="submit" name="add-category">
                             Add Category
                         </button>
                     </form>
-                    <h3>Categories</h3>
-                    <?php
-                    $categories->data_seek(0);
-                    while ($category = $categories->fetch_assoc()) {
-                        ?>
-                        <p>
-                            <?php echo $category['name']; ?>
-                            <a href="admin.php?delete-category=<?php echo $category['id']; ?>"
-                                onclick="return confirm ('Delete this category?')">Delete</a>
-                        </p>
-                    <?php } ?>
-
+                    <div class="admin-categories">
+                        <h3>Categories</h3>
+                        <?php
+                        $categories->data_seek(0);
+                        while ($category = $categories->fetch_assoc()) {
+                            ?>
+                            <p>
+                                <?php echo $category['name']; ?>
+                                <a href="admin.php?delete-category=<?php echo $category['id']; ?>"
+                                    onclick="return confirm ('Delete this category?')">Delete</a>
+                            </p>
+                        <?php } ?>
+                    </div>
                 </div>
-            
-            
 
+                <!---interior do botao categories---->
+                <div id="categories" class="admin-section">
+                    <h2>Filter Category</h2>
+                    <div class="admin-categories">
+                        <a href="admin.php">All Products</a>
+                        <?php $categories->data_seek(0);
+                        while ($category = $categories->fetch_assoc()) {
+                            ?>
+                            <a href="admin.php?category=<?php echo $category['id']; ?>">
+                                <?php echo $category['name']; ?>
+                            </a>
+                        <?php } ?>
+                    </div>
+                </div>
+
+            </div>
 
             <hr>
 
-            <div class="admin-categories">
+                <h2>Products</h2>
+                <div class="admin-products">
+                    <?php
+                    if (isset($_GET['search']) && !empty($_GET['search'])) {
+                        $search = $_GET['search'];
+                        $products = $conn->query("SELECT * FROM products WHERE name LIKE '%$search%'");
+                    } elseif (isset($_GET['category'])) {
+                        $category_id = $_GET['category'];
+                        $products = $conn->query("SELECT * FROM products WHERE category_id = '$category_id'");
+                    } else {
+                        $products = $conn->query("SELECT * FROM products");
+                    }
+                    while ($product = $products->fetch_assoc()) { ?>
+                        <div class="admin-product">
+                            <img src="<?php echo $product['image']; ?>" width="100">
+                            <h3><?php echo $product['name']; ?></h3>
+                            <p>CHF <?php echo $product['price']; ?></p>
+                            <p><?php echo $product['badge']; ?></p>
+                            <p>Stock: <?php echo $product['stock']; ?></p>
+                            <a href="edit.php?id=<?php echo $product['id']; ?>">
+                                Edit
+                            </a>
+                            <a href="admin.php?delete=<?php echo $product['id']; ?>"
+                                onclick="return confirm('Tem a certeza que quer apagar este produto?')">
+                                Delete
+                            </a>
 
+
+                        </div>
+                    <?php } ?>
+                </div>
             
-            <h2>Products</h2>
-            <div class="admin-products">
-                <?php
-                if (isset($_GET['search']) && !empty($_GET['search'])) {
-                    $search = $_GET['search'];
-                    $products = $conn->query("SELECT * FROM products WHERE name LIKE '%$search%'");
-                } elseif (isset($_GET['category'])) {
-                    $category_id = $_GET['category'];
-                    $products = $conn->query("SELECT * FROM products WHERE category_id = '$category_id'");
-                } else {
-                    $products = $conn->query("SELECT * FROM products");
-                }
-                while ($product = $products->fetch_assoc()) { ?>
-                    <div class="admin-product">
-                        <img src="<?php echo $product['image']; ?>" width="100">
-                        <h3><?php echo $product['name']; ?></h3>
-                        <p>CHF <?php echo $product['price']; ?></p>
-                        <p><?php echo $product['badge']; ?></p>
-                        <p>Stock: <?php echo $product['stock']; ?></p>
-                        <a href="edit.php?id=<?php echo $product['id']; ?>">
-                            Edit
-                        </a>
-                        <a href="admin.php?delete=<?php echo $product['id']; ?>"
-                            onclick="return confirm('Tem a certeza que quer apagar este produto?')">
-                            Delete
-                        </a>
-
-
-                    </div>
-                <?php } ?>
-            </div>
-            </div>
-            </div>
-            </div>
         </div>
+    </div>
+    </div>
     </div>
 
     <script>
 
-      function Showtab(tabId) {
+        function Showtab(tabId) {
 
-    let tab = document.getElementById(tabId);
+            let tab = document.getElementById(tabId);
+            let tabsContent = document.getElementById("tabs-content");
 
-    // Fecha se já estiver aberto
-    if (tab.style.display === "block") {
-        tab.style.display = "none";
+            // Fecha se já estiver aberto
+            if (tab.style.display === "block") {
+                tab.style.display = "none";
+                tabsContent.style.display ="none";
 
-        document.getElementById("search-wrapper").classList.remove("active");
-        document.getElementById("add-product-wrapper").classList.remove("active");
-        document.getElementById("add-category-wrapper").classList.remove("active");
-        document.getElementById("categories-wrapper").classList.remove("active");
+                document.getElementById("search-wrapper").classList.remove("active");
+                document.getElementById("add-product-wrapper").classList.remove("active");
+                document.getElementById("add-category-wrapper").classList.remove("active");
+                document.getElementById("categories-wrapper").classList.remove("active");
 
+                return;
+            }
+
+            // Fecha todos os painéis
+            document.getElementById("search-products").style.display = "none";
+            document.getElementById("add-product").style.display = "none";
+            document.getElementById("add-category").style.display = "none";
+            document.getElementById("categories").style.display = "none";
+
+            // Remove molduras ativas
+            document.getElementById("search-wrapper").classList.remove("active");
+            document.getElementById("add-product-wrapper").classList.remove("active");
+            document.getElementById("add-category-wrapper").classList.remove("active");
+            document.getElementById("categories-wrapper").classList.remove("active");
+
+            // Abre o painel clicado
+            tabsContent.style.display = "block";
+            tab.style.display = "block";
+
+            // Ativa a moldura correspondente
+            if (tabId === "search-products") {
+                document.getElementById("search-wrapper").classList.add("active");
+            }
+
+            if (tabId === "add-product") {
+                document.getElementById("add-product-wrapper").classList.add("active");
+            }
+
+            if (tabId === "add-category") {
+                document.getElementById("add-category-wrapper").classList.add("active");
+            }
+
+            if (tabId === "categories") {
+                document.getElementById("categories-wrapper").classList.add("active");
+            }
+        }
+        document.addEventListener("click", function(event) {
+
+    const tabsContent = document.getElementById("tabs-content");
+    const adminTabs = document.getElementById("admin-tabs");
+
+    if (
+        adminTabs.contains(event.target) ||
+        tabsContent.contains(event.target)
+    ) {
         return;
     }
 
-    // Fecha todos os painéis
+    // Esconde todos os painéis
     document.getElementById("search-products").style.display = "none";
     document.getElementById("add-product").style.display = "none";
     document.getElementById("add-category").style.display = "none";
     document.getElementById("categories").style.display = "none";
 
-    // Remove molduras ativas
+    // Esconde o contentor principal
+    tabsContent.style.display = "none";
+
+    // Remove os estados ativos
     document.getElementById("search-wrapper").classList.remove("active");
     document.getElementById("add-product-wrapper").classList.remove("active");
     document.getElementById("add-category-wrapper").classList.remove("active");
     document.getElementById("categories-wrapper").classList.remove("active");
 
-    // Abre o painel clicado
-    tab.style.display = "block";
-
-    // Ativa a moldura correspondente
-    if (tabId === "search-products") {
-        document.getElementById("search-wrapper").classList.add("active");
-    }
-
-    if (tabId === "add-product") {
-        document.getElementById("add-product-wrapper").classList.add("active");
-    }
-
-    if (tabId === "add-category") {
-        document.getElementById("add-category-wrapper").classList.add("active");
-    }
-
-    if (tabId === "categories") {
-        document.getElementById("categories-wrapper").classList.add("active");
-    }
-}
+});
 
 
     </script>
